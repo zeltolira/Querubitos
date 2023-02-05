70bb1b7e0a44362ace0cd9c6130548c4a944031f
Zelto Lira <you@example.com>
2023-02-05 16:09:38 -0300

    Inicia projeto

---
+++ src/main/java/br/com/querubitos/querubitos/QuerubitosApplication.java
@@ -0,0 +1,13 @@
+package br.com.querubitos.querubitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerubitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerubitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/querubitos/querubitos/QuerubitosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.querubitos.querubitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerubitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
