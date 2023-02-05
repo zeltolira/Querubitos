a4871d60bcd935a06036f7f7e7042c655fe5f80b
Zelto Lira <you@example.com>
2023-02-05 16:58:26 -0300

    implementa api-adole

---
+++ src/main/java/br/com/querubitos/adole/domain/Adolescente.java
@@ -0,0 +1,45 @@
+package br.com.querubitos.adole.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@Entity
+public class Adolescente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idAdolescente;
+	@NotBlank
+	private String nomeAdolescente;
+	@NotBlank
+	private LocalDate dataNascimento;
+	@NotBlank
+	private String celular;
+	@NonNull
+	private Sexo sexo;
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraultimaAlteracao;
+	
+	public Adolescente(@NotBlank String nomeAdolescente, @NotBlank LocalDate dataNascimento, @NotBlank String celular,
+			Sexo sexo) {
+		this.idAdolescente = UUID.randomUUID();
+		this.nomeAdolescente = nomeAdolescente;
+		this.dataNascimento = dataNascimento;
+		this.celular = celular;
+		this.sexo = sexo;
+	}
+	
+	
+}
--- src/main/java/br/com/querubitos/querubitos/QuerubitosApplication.java
+++ src/main/java/br/com/querubitos/querubitos/QuerubitosApplication.java
@@ -2,12 +2,21 @@ package br.com.querubitos.querubitos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class QuerubitosApplication {
-
+	
+	@GetMapping
+	public String getHomeTest() {
+		return "Adolescentes - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QuerubitosApplication.class, args);
 	}
-
 }
