a1ab191c166633347da724a232eebe4106fb489d
Zelto Lira <you@example.com>
2023-03-15 06:06:40 -0300

    acrescenta criacao automatica de tabela no banco de dados no Querubitos.java

--- src/main/java/br/com/querubitos/querubitos/domain/Querubitos.java
+++ src/main/java/br/com/querubitos/querubitos/domain/Querubitos.java
@@ -3,14 +3,16 @@ package br.com.querubitos.querubitos.domain;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import br.com.querubitos.querubitos.application.api.QuerubitoRequest;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,41 +20,53 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Querubito {
-
+@Table(name = "querubitos")
+public class Querubitos {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(columnDefinition = "uuid", name = "idQuerubito", updatable = false, unique = true, nullable = false)
 	private UUID idQuerubito;
 	@NotNull
-	@Enumerated(EnumType.STRING)
+	private UUID idAdolescenteRecebedor;
+	@NotNull
 	private EstudoDiario estudoDiario;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private ParticipacaoClasse participacaoClasse;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private ParticipacaoExternas participacaoExternas;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private ParticipacaoDesafios participacaoDesafios;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private ParticipacaoCultos participacaoCultos;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private Quiz quiz;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private Visitantes_amigos visitantes_amigos;
 	@NotNull
-	@Enumerated(EnumType.STRING)
 	private Intercambio intercambio;
 	@NotNull
 	private Integer extra;
 	@NotNull
 	private Integer desobediencia;
-
+	
+	
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataHoraultimaAlteracao;
+
+	public Querubitos(UUID idAdolescente, @Valid QuerubitoRequest querubitoRequest) {
+		this.idAdolescenteRecebedor = idAdolescente;
+		this.estudoDiario = querubitoRequest.getEstudoDiario();
+		this.participacaoClasse = querubitoRequest.getParticipacaoClasse();
+		this.participacaoExternas = querubitoRequest.getParticipacaoExternas();
+		this.participacaoDesafios = querubitoRequest.getParticipacaoDesafios();
+		this.participacaoCultos = querubitoRequest.getParticipacaoCultos();
+		this.quiz = querubitoRequest.getQuiz();
+		this.visitantes_amigos = querubitoRequest.getVisitantes_amigos();
+		this.intercambio = querubitoRequest.getIntercambio();
+		this.extra = querubitoRequest.getExtra();
+		this.desobediencia = querubitoRequest.getDesobediencia();
+		this.dataHoraDoCadastro = LocalDateTime.now();
+	}
+
 }
