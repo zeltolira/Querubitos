ce8b3a1a86d0c0228bf6884f7f6a1fe4723ffb5e
Zelto Lira <you@example.com>
2023-04-12 07:27:00 -0300

    implementa controller e fluxo de service

--- src/main/java/br/com/querubitos/tipoAcao/application/api/QuizController.java
+++ src/main/java/br/com/querubitos/tipoAcao/application/api/QuizController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.querubitos.tipoAcao.application.api.request.QuizRequest;
 import br.com.querubitos.tipoAcao.application.api.response.QuizResponse;
+import br.com.querubitos.tipoAcao.application.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -16,11 +17,15 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class QuizController implements QuizAPI {
 
+	private final QuizService quizService;
+
 	@Override
 	public QuizResponse postQuiz(UUID idAdolescente, @Valid QuizRequest quizRequest) {
 		log.info("[inicia] QuizController - postQuiz");
+		log.info("[idAdolescente] {}, idAdolescente");
+		QuizResponse quiz = quizService.criaQuiz(idAdolescente, quizRequest);
 		log.info("[finaliza] QuizController - postQuiz");
-		return null;
+		return quiz;
 	}
 
 }
